d47b3f3b5e39fd0153187a6d0eee4b3b56b880e4
Hesham El-Sewify <computer.boy71@yahoo.com>
2020-12-10 07:00:35 +0200

    Inherit omni stuff

--- omni_m51.mk
+++ omni_m51.mk
@@ -16,11 +16,18 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Inherit from m51 device makefile
 $(call inherit-product, device/samsung/m51/device.mk)
 
-$(call inherit-product, vendor/pb/config/common.mk)
+#$(call inherit-product, vendor/pb/config/common.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := omni_m51
